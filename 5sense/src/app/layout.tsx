985b26d8ec6f25fe4ec557b6224a22288aeaf31d
yeun38 <choh1997@naver.com>
2023-10-23 16:00:12 +0900

    view: 메인 화면 변경 사항 반영 및 컴포넌트 재구성

--- 5sense/src/app/layout.tsx
+++ 5sense/src/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from 'next'
 import './globals.css'
 import Navbar from '@/components/Navbar'
+import Image from 'next/image'
+import mainlogo from '../assets/logo/mainlogo.svg'
+import profie from '../assets/images/profile.png'
+import ProfileBtn from '../components/btn/ProfileBtn'
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,9 +20,44 @@ export default function RootLayout({
     <html lang="en">
       <body className="flex w-full overflow-auto justify-center ">
         <main className="w-full max-w-screen-3xl min-w-screen-md bg-primary max-h-screen-h-3xl">
-          <header className="w-full h-[99px] bg-primary2">서비스이름</header>
+          <div className="Sidebar w-[1920px] h-[469px] relative bg-gradient-to-b from-indigo-500 to-violet-500">
+            <div className="Info w-[40.67px] h-[40.67px] left-[24px] top-[48px] absolute">
+              <div className="Logo flex flex-row items-center gap-[6px]">
+                <Image
+                  src={mainlogo}
+                  width={52}
+                  height={52}
+                  alt="mainlogo"
+                ></Image>
+                <span className="Sense text-slate-50 text-xl font-bold font-['Poppins']">
+                  5sense
+                </span>
+              </div>
+              <div className="UserInfo flex flex-col items-center w-[288px] h-[251px] mt-20">
+                <Image
+                  src={profie}
+                  width={88}
+                  height={88}
+                  alt="profile"
+                  className="Profile1 w-[88px] h-[88px] rounded-full"
+                />
+                <div className="content flex flex-col items-center gap-[8px] mb-7">
+                  <h3 className="Text text-center text-white text-[21px] font-bold font-['Pretendard'] leading-loose">
+                    매직기구필라테스
+                  </h3>
+                  <span className="Text text-center text-indigo-50 text-sm font-medium font-['Pretendard'] leading-[14px]">
+                    031-706-1281
+                  </span>
+                  <span className="Text w-72 text-center text-indigo-50 text-xs font-medium font-['Pretendard'] leading-3">
+                    경기 성남시 분당구 판교로256번길 19
+                  </span>
+                </div>
+                <ProfileBtn />
+              </div>
+            </div>
+          </div>
+
           <section className="flex flex-row">
-            <div className="flex-none w-[382px]">학원정보</div>
             <div>
               <Navbar />
               <div className="w-[1490px] h-[874px] bg-white pt-10 pl-10">
