76cfdffa3aa94846b0597e45dc9459eda1be9bd7
shcho <acckr0604@gmail.com>
2023-10-25 21:30:46 +0900

    AADMIN-60 feat: 카카오 로그인 기능 구현 방식 REST_API로 변경

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,25 +6,30 @@ import kakaoLogo from '../../assets/logo/kakaoLogo.svg';
 import naverLogo from '../../assets/logo/naverLogo.svg';
 import googleLogo from '../../assets/logo/googleLogo.svg';
 
-declare global { // Kakao 함수를 전역에서 사용할 수 있도록 선언
+/* declare global { // Kakao 함수를 전역에서 사용할 수 있도록 선언
     interface Window {
       Kakao: any;
       naver: any;
     }
-  }
+  } */
 
 export default function Login() {
 
-    function kakaoInit() { // 페이지가 로드되면 실행
-        window.Kakao.init('6bc5fda37065e978c7091b6f1987a7c2');
-        console.log(window.Kakao.isInitialized());
-      }
+    const KAKAO_REST_API_KEY = process.env.REST_API_KEY;
+    const KAKAO_REDIRECT_URI = process.env.REDIRECT_URI;
+    console.log(KAKAO_REST_API_KEY, KAKAO_REDIRECT_URI)
+    const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${KAKAO_REST_API_KEY}&redirect_uri=${KAKAO_REDIRECT_URI}`
 
-    function kakaoLogin() {
+    /* function kakaoInit() { // 페이지가 로드되면 실행
+        window.Kakao.init();
+        console.log(window.Kakao.isInitialized());
+      } */
+
+    /* function kakaoLogin() {
     window.Kakao.Auth.authorize({
-        redirectUri: 'http://localhost:3000/kakao_login', 
+        redirectUri: ', 
     });
-    }
+    } */
 
     function naverLogin() {
         //window.naver('wCIY4BCK_aQX4TYCnq8T', 'http://localhost:3000/naver_login');
@@ -34,20 +39,22 @@ export default function Login() {
 
     return(
         <>
-        <Script id='kakao_SDK' src="https://t1.kakaocdn.net/kakao_js_sdk/2.4.0/kakao.min.js" 
+        {/* <Script id='kakao_SDK' src="https://t1.kakaocdn.net/kakao_js_sdk/2.4.0/kakao.min.js" 
         integrity="sha384-mXVrIX2T/Kszp6Z0aEWaA8Nm7J6/ZeWXbL8UpGRjKwWe56Srd/iyNmWMBhcItAjH" 
         crossOrigin="anonymous" onLoad={kakaoInit}>
-        </Script>
+        </Script> */}
         <Script type="text/javascript" src="https://static.nid.naver.com/js/naverLogin_implicit-1.0.3.js" charSet="utf-8"></Script>
         <Script type="text/javascript" src="http://code.jquery.com/jquery-1.11.3.min.js"></Script>
         <div className='w-[430px] h-[250px] flex flex-col items-start gap-2'>
+            <a href={KAKAO_AUTH_URL}>
             <div className='flex w-[430px] h-[52px] rounded-md bg-[#FFE812]'>
                 <Image src={kakaoLogo} width={48} height={48} alt='kakaoLogo' />
-                <div className='font-semibold w-[190px] h-[16px] leading-4 mt-[18px] mx-auto' onClick={kakaoLogin}>카카오 계정으로 로그인</div>
+                <div className='font-semibold w-[190px] h-[16px] leading-4 mt-[18px] mx-auto' >카카오 계정으로 로그인</div>
             </div>
-            <div className='flex w-[430px] h-[52px] rounded-md border border-[#2BB500]'>
+            </a>
+            <div className='flex w-[430px] h-[52px] rounded-md border border-[#2BB500]' onClick={naverLogin}>
                 <Image src={naverLogo} width={48} height={48} alt='naverLogo' />
-                <div className='mx-auto font-semibold h-[16px] leading-4 mt-[18px] text-[#2BB500]' onClick={naverLogin}>네이버 계정으로 로그인</div>
+                <div className='mx-auto font-semibold h-[16px] leading-4 mt-[18px] text-[#2BB500]' >네이버 계정으로 로그인</div>
             </div>
             <div className='flex w-[430px] h-[52px] rounded-md border border-[#6B7280]'>
                 <Image className='p-[15px]' src={googleLogo} width={48} height={48} alt='googleLogo' />
