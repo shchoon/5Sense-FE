75391e46e39fd0965c49e54c76ae98de458c0865
shcho <acckr0604@gmail.com>
2023-10-30 13:26:47 +0900

    chore: next.config.js 파일 수정

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-    async rewrites() {
-        return [
-        {
-            source: "/:path*",
-            destination: "http://13.209.77.49:4000/:path*",
-        },
-        ];
-    },
+    
 }
 
 module.exports = nextConfig
