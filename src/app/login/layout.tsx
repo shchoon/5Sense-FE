e1fb5f1cce68b54ba28e234856b4f2bbfbba771d
shcho <acckr0604@gmail.com>
2023-11-03 16:06:17 +0900

    AADMIN-59 view: 로그인 UI 수정

--- src/app/login/layout.tsx
+++ src/app/login/layout.tsx
@@ -10,7 +10,7 @@ export default function loginLayout({
     return (
       <>
       <div className='flex justify-center h-[1080px] items-center py-[272px] w-[1920px]  bg-[#F8FAFD]'>
-        <div className='flex items-start flex-col w-[442px]  h-[476px] gap-14 px-[6px] pt-3 pb-2 border rounded-2xl border-[#E5E7EB] bg-[#FFF] shadow-[-3px_5px_20px_0px_rgba(0,0,0,0.03)]'>
+        <div className='flex items-start flex-col w-[442px]  h-[426px] gap-14 px-[6px] pt-3 pb-2 border rounded-2xl border-[#E5E7EB] bg-[#FFF] shadow-[-3px_5px_20px_0px_rgba(0,0,0,0.03)]'>
             <div className='flex items-center gap-[56px] mx-auto'>
                 <div className='flex items-center flex-col w-[383px] h-[150px] gap-[30px]'>
                         <Image src={mainLogo} width={60} height={60} className="p-[5px]" alt="main-logo"/>
