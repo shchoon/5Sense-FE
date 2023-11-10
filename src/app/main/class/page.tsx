d3128b80650c7269dc77b711bb1143c66095fcd3
조영은 <86697114+yeun38@users.noreply.github.com>
2023-11-11 01:30:24 +0900

    View/classlist (#11)
    * view: 레이아웃 반응형으로 그리기
    
    * view:반응형 레이아웃 및 클래스 목록 반응형 구현(70%)
    
    * view:classlist grid 구현중
    
    * Merge branch 'dev' of https://github.com/PJ-5Sense/5Sense-FE into view/classlist

--- src/app/main/class/page.tsx
+++ src/app/main/class/page.tsx
@@ -1,9 +1,9 @@
-"use client"
-import { useSearchParams } from 'next/navigation';
-import dropDown from '../../../assets/icons/dropDown.svg';
-import upIcon from '../../../assets/icons/upIcon.svg';
-import Image from 'next/image';
-import ClassFilter from '@/components/classFilter/classFilter';
+'use client'
+import { useSearchParams } from 'next/navigation'
+import dropDown from '../../../assets/icons/dropDown.svg'
+import upIcon from '../../../assets/icons/upIcon.svg'
+import Image from 'next/image'
+import ClassFilter from '@/components/classFilter/classFilter'
 
 export default function ClassPage() {
   const data = [
@@ -159,17 +159,17 @@ export default function ClassPage() {
     }
   ]
 
-
   return (
     <div className="w-full h-full px-12 py-[60px] box-border">
       <h1 className="menu-title">클래스 관리</h1>
-      <div className="container w-full h-full grid grid-cols-3 gap-[20px]">
+      <ClassFilter />
+      <div className="container w-full max-w-[1872px] grid grid-cols-2 2xl:grid-cols-3 gap-[20px] mt-[87px]">
         {data.map(({ title, instructor, userCnt }) => (
           <div
             key={title}
             className="h-56 px-6 py-8 bg-white rounded-lg shadow border border-gray-200 flex-col justify-start items-start gap-16 inline-flex"
           >
-            <div className=" self-stretch text-gray-900 text-xl font-semibold font-['Pretendard'] leading-[30px]">
+            <div className="self-stretch text-gray-900 text-xl font-semibold font-['Pretendard'] leading-[30px]">
               {title}
             </div>
             <div className="Frame814119 self-stretch justify-between items-end inline-flex">
