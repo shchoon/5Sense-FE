9d4f7b9c4bbee80a8f916b5a56ea00cea746efeb
shcho <acckr0604@gmail.com>
2023-11-16 16:50:16 +0900

    메인페이지(주) -> 날짜 & 주,일,월 탭 반응형 레이아웃 구현

--- src/app/main/week/layout.tsx
+++ src/app/main/week/layout.tsx
@@ -3,5 +3,5 @@ export default function LectureDetailLayout({
   }: {
     children: React.ReactNode
   }) {
-    return <div>{children}</div>
+    return <div className="relative">{children}</div>
   }
--- src/app/main/week/page.tsx
+++ src/app/main/week/page.tsx
@@ -8,6 +8,7 @@ export default function MainPageWeek() {
     console.log(width)
     
     return(
+        <>
         <div className="w-full h-screen flex flex-col items-start rounded-2xl shadow-mainPage bg-white xl:px-12 lg:px-6 md:px-12 px-6 pt-[60px] gap-4">
             <div className="mx-auto w-full max-w-[1480px] h-[251px] flex lg:gap-6 md:gap-0">
                 <div className="w-full 2xl:max-w-[593px] 2xl:mr-0 xl:max-w-[361px] xl:mr-0 lg:max-w-[321px] lg:min-w-[321px] lg:mr-0 mr-4 h-full 
@@ -50,6 +51,7 @@ export default function MainPageWeek() {
                         <Image src={graph} width={118} height={118} alt="" />
                     </div>
                 </div>
+                {/* 메모장 */}
                 {width > 1024 ? 
                 <div className="max-w-[246px]  lg:w-full md:w-0 h-full border rounded-xl border-gray-200 p-6 ">
                     <div className="text-gray-600 text-base font-medium font-['Pretendard'] leading-normal">센터 메모</div>
@@ -63,13 +65,31 @@ export default function MainPageWeek() {
 
                 </div> */}
             </div>
+            {/* 메모장 태블릿 */}
             {width <= 1024 ? 
             <div className="w-full h-[144px] border rounded-xl border-gray-200 p-5 ">
                 <div className="text-gray-600 text-base font-medium font-['Pretendard'] leading-normal">센터 메모</div>
                 <textarea placeholder="메모를 적어보세요" className="w-full border-0 pl-0 focus:ring-0" />
             </div> : null}
-            
-            
         </div>
+        {/* 날짜 & 주,일,월 탭 1000px 이상*/}
+        {width >= 1000 ? 
+        <div className="absolute top-[537px] lg:top-[391px] md:top-[537px] w-full flex justify-center xl:px-12 lg:px-6 md:px-12 px-6">
+            <div className="relative flex gap-[138px] items-center w-full max-w-[1016px]  border h-[52px] xl:max-w-[1016px] lg:max-w-[936px] md:w-full ">
+                <div className="mx-auto w-[420px] h-full bg-slate-400">tab 1</div>
+                <div className="absolute right-0 w-[160px] h-[44px] bg-blue-200">tab 2</div>
+            </div>
+        </div> : null}
+        {/* 날짜 & 주,일,월 탭 1000px 미만 */}
+        {width < 1000 ? 
+        <div className="absolute top-[537px] w-full h-[52px] flex justify-end md:pr-12 pr-6">
+            <div className="w-[492px] h-full border flex items-center gap-5">
+                <div className="mx-auto w-[420px] h-full bg-slate-400">tab 1di</div>
+                <div className="w-[160px] h-[44px] bg-blue-200">tab 2</div>
+            </div>
+        </div>
+        : null}
+            
+        </>
     )
 }
