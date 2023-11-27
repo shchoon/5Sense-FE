4107d3910780af9da977f769865784deb2ebaf88
조영은 <86697114+yeun38@users.noreply.github.com>
2023-11-27 11:05:25 +0900

    View/navbar (#19)
    * view: 레이아웃 반응형으로 그리기
    
    * view:반응형 레이아웃 및 클래스 목록 반응형 구현(70%)
    
    * view:classlist grid 구현중
    
    * Merge branch 'dev' of https://github.com/PJ-5Sense/5Sense-FE into view/classlist
    
    * AADMIN-83 feat:클래스 정보 입력 기능 구현
    
    * feat: router 폴더 단계 수정 및 반응형 최소넓이 버그 해결
    
    * feat:네브바 ative 시 자식 컴포넌트도 포함되도록 수정
    
    * feat: input컴포넌트 react-hook 컴포넌트 구현
    
    * feat: 레이아웃으로 폴더명 변경 및 전역 스타일 추가
    
    * feat: register페이지 그리기 및 기능 구현
    
    * fix: package-lock파일 통일
    
    * package-lock.json 변경
    
    * AADMIN-147 모달 추가 및 레이아웃 구조 재수정
    
    * AADMIN-91 feat: 네브바 기능 구현 완료

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -5,66 +5,103 @@ import home from '@/assets/icons/home.svg'
 import user from '@/assets/icons/user.svg'
 import receipt from '@/assets/icons/receipt.svg'
 import profileCard from '@/assets/icons/profile-card.svg'
+import rectangle from '@/assets/images/Rectangle.svg'
 import Image from 'next/image'
 import { usePathname } from 'next/navigation'
 import { useState } from 'react'
+import { Router } from 'next/router'
+import path from 'path'
 
-const menu = [
+type Menu = {
+  id: string
+  src: string
+  slug: string
+  title: string
+  avtive: boolean
+}
+
+const menu: Menu[] = [
   {
-    id: 0,
+    id: '0',
     src: home,
     slug: '/home',
-    title: '메인 홈'
+    title: '메인 홈',
+    avtive: false
   },
   {
-    id: 1,
+    id: '1',
     src: openBook,
     slug: '/class',
-    title: '클래스 관리'
+    title: '클래스 관리',
+    avtive: false
   },
   {
-    id: 2,
+    id: '2',
     src: user,
     slug: '/student',
-    title: '수강생 관리'
+    title: '수강생 관리',
+    avtive: false
   },
   {
-    id: 3,
+    id: '3',
     src: profileCard,
     slug: '/instructor',
-    title: '강사 관리'
+    title: '강사 관리',
+    avtive: false
   },
   {
-    id: 4,
+    id: '4',
     src: receipt,
     slug: '/pay',
-    title: '청구/납부'
+    title: '청구/납부',
+    avtive: false
   }
 ]
-// flex하면 부모 크기에 맞춰지나?
+
 export default function Navbar() {
   const pathname = usePathname()
-  const isActive = useState(false)
-  console.log(pathname)
 
   return (
-    <div className="flex w-[720px] xl:w-[879px] translate-y-[16px]">
+    <div className="flex w-[720px] xl:w-[879px] gap-[1px]">
       {menu.map(menu => (
-        <Link
-          className={`link ${
-            pathname.includes(menu.slug)
-              ? 'nav-active'
-              : 'bg-primary-400 text-primary-200 text-base rounded-tl-xl rounded-tr-xl'
-          } relative flex justify-center w-[168px] h-[70px] leading-[60px] text-center font-bold font-['Pretendard']`}
-          href={`${menu.slug}`}
-          key={menu.id}
-        >
-          <Image
-            className="relative w-[20px] h-[20px] top-[20px] right-[10px]"
-            src={menu.src}
-            alt={menu.slug}
-          />
-          {menu.title}
+        <Link href={`${menu.slug}`} key={menu.id}>
+          <div
+            className={`flex justify-center w-[135px] h-14  ${
+              pathname.includes(menu.slug)
+                ? 'w-[170px] xl:w-[205px]'
+                : 'w-[135px] xl:w-[168px] bg-primary-400'
+            } xl:h-[70px] rounded-tl-xl rounded-tr-xl translate-y-[14px] xl:translate-y-[17px]`}
+          >
+            <div className="content w-[135px] h-[42px] xl:w-[168px] xl:h-[54px] flex justify-center items-center gap-[10px]">
+              <Image
+                className="top-4 left-[37.5px]"
+                src={menu.src}
+                alt={menu.title}
+              />
+              <span
+                className={`leading-[60px] text-primary-300 font-bold text-[13px] xl:text-base`}
+              >
+                {menu.title}
+              </span>
+            </div>
+          </div>
+          {pathname.includes(menu.slug) ? (
+            <div className="absolute top-[124px] lg:top-0 translate-y-2 z-30">
+              <Image
+                src={rectangle}
+                alt="active"
+                className="w-[177px] h-[50px] xl:w-[220px] xl:h-[62px]"
+              />
+              <div className="content w-[135px] h-[42px] xl:w-[205px] xl:h-[54px] flex justify-center items-center gap-[10px] translate-y-[-42px] xl:translate-y-[-54px]">
+                <Image src={menu.src} alt={menu.title} />
+                <span className="bold-500 text-base xl:text-lg leading-[42px] xl:leading-[60px]">
+                  {menu.title}
+                </span>
+              </div>
+            </div>
+          ) : (
+            ''
+          )}
         </Link>
       ))}
     </div>
