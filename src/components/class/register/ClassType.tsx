f8aa0b6ca4f11ba48385965742c812868448d90c
조영은 <86697114+yeun38@users.noreply.github.com>
2023-12-25 23:52:29 +0900

    Feat/class register (#29)
    * feat: 클래스 리스트 디테일 수정

--- src/components/class/register/ClassType.tsx
+++ src/components/class/register/ClassType.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import ClassFilter from '@/components/classFilter/classFilter'
 import { useSelect } from '@/hooks/useSelect'
 import { ChangeEvent, useEffect, useMemo, useState } from 'react'
 import DatePicker, { CalendarContainer } from 'react-datepicker'
@@ -13,7 +12,6 @@ import Schedule from './Schedule'
 import 'react-datepicker/dist/react-datepicker.css'
 import ko from 'date-fns/locale/ko'
 import Image from 'next/image'
-import SelectForm from '@/components/SelectForm'
 
 export default function ClassType() {
   const [one, setOne] = useState<string>('')
@@ -67,18 +65,6 @@ export default function ClassType() {
   const addComma = (price: any) => {
     return price?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
   }
-
-  // const convertToTenThousand = (numberString: string) => {
-  //   // 콤마 제거 및 숫자로 변환
-  //   const number = parseInt(numberString.replace(/,/g, ''), 10)
-
-  //   // 만원 단위로 변환
-  //   if (isNaN(number) || number === 0) {
-  //     return 0
-  //   } else {
-  //     return (number / 10000).toFixed(1)
-  //   }
-  // }
 
   function getKoreanNumber(num: string) {
     let number = parseInt(num)
@@ -147,39 +133,33 @@ export default function ClassType() {
     let str = value.replaceAll(',', '')
     setMoney(str)
   }
-  const MyContainer = ({ className, children }: any) => {
-    const [isDropdownView, setDropdownView] = useState(false)
-    const [isSelectWeek, setIsSelectWeek] = useState(false)
-    const [startH, handleStartH, activeSH, handleActiveSH] =
-      useSelect('시간 선택')
-    const [startM, handleStartM, activeSM, handleActiveSM] = useSelect('0')
-    const [endH, handleEndH, activeEH, handleActiveEH] = useSelect('0')
-    const [endM, handleEndM, activeEM, handelActiveEM] = useSelect('0')
-    const [weekend, handleWeekend, activeWeek, handleA] = useSelect('')
-    const hourArray = Array.from({ length: 23 }, (_, index) => index + 1)
-    const minArray = Array.from({ length: 12 }, (_, index) => index * 5)
-    const [select, setSelect] = useState<any>([])
-    const week = ['월', '화', '수', '목', '금', '토', '일']
 
-    const sortDow = (weekDay: any[]) => {
-      const weekDaySorter: any = { 월: 1, 화: 2, 수: 3, 목: 4, 금: 5 }
-      weekDay.sort(function sortByWeekDay(a: number, b: number): number {
-        return weekDaySorter[a] - weekDaySorter[b]
-      })
-      return weekDay
-    }
+  const [isSelectWeek, setIsSelectWeek] = useState(false)
 
-    const handleClickWeek = (li: string) => {
-      if (select.includes(li)) {
-        setSelect(sortDow(select.filter((item: any) => item !== li)))
-      } else {
-        setSelect(sortDow([...select, li]))
-      }
-    }
+  const hourArray = Array.from({ length: 23 }, (_, index) => index + 1)
+  const minArray = Array.from({ length: 12 }, (_, index) => index * 5)
+  const [select, setSelect] = useState<any>([])
+  const week = ['월', '화', '수', '목', '금', '토', '일']
 
-    const handleClickContainer2 = () => {
-      setIsSelectWeek(!isSelectWeek)
+  const sortDow = (weekDay: any[]) => {
+    const weekDaySorter: any = { 월: 1, 화: 2, 수: 3, 목: 4, 금: 5 }
+    weekDay.sort(function sortByWeekDay(a: number, b: number): number {
+      return weekDaySorter[a] - weekDaySorter[b]
+    })
+    return weekDay
+  }
+
+  const handleClickWeek = (li: string) => {
+    if (select.includes(li)) {
+      setSelect(sortDow(select.filter((item: any) => item !== li)))
+    } else {
+      setSelect(sortDow([...select, li]))
     }
+  }
+
+  const handleClickContainer2 = () => {
+    setIsSelectWeek(!isSelectWeek)
+  }
 
   const activeButton = (content: string, isActive: boolean) => {
     return (
@@ -199,7 +179,7 @@ export default function ClassType() {
   return (
     <div className="class-box">
       <div className="Title gray-900-bold text-xl">클래스 유형</div>
-      <div className="flex w-[592px] h-[52px] p-1.5 rounded-md border border-gray-300 ">
+      <div className="flex w-full h-[52px] p-1.5 rounded-md border border-gray-300 ">
         {activeButton('기간반', tab)}
         {activeButton('회차반', !tab)}
       </div>
@@ -221,7 +201,7 @@ export default function ClassType() {
             <p className="gray-800-semibold">일정</p>
             <button
               onClick={createComponent}
-              className="w-[592px] h-[52px] px-6 py-3.5 btn-line-purple"
+              className="w-full h-[52px] px-6 py-3.5 btn-line-purple"
             >
               일정 추가
             </button>
