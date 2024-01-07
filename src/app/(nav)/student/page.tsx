e075ad26bbfefb78d6430d014a1773223e15b646
shcho <acckr0604@gmail.com>
2024-01-07 19:08:41 +0900

    feat: 기존 유저 여부에 따른 라우팅 구현

--- src/app/(nav)/student/page.tsx
+++ src/app/(nav)/student/page.tsx
@@ -137,18 +137,20 @@ export default function StudentPage() {
   function onClickX() {
     setSearchInput('')
     fetchApi('/students?searchBy=none', 'GET').then(result => {
-      let cursorIndex = result.data.students.length - 1
-      console.log(cursorIndex)
-      setStudentData((preStudentData: studentType[]) => [
-        ...preStudentData,
-        ...result.data.students
-      ])
-      setPostVariable((prePostVariable: postVariableType) => ({
-        ...prePostVariable,
-        page: result.data.meta.page,
-        cursor: result.data.students[cursorIndex].id,
-        hasNextPage: result.data.meta.hasNextPage
-      }))
+      if (result.data.students.length !== 0) {
+        let cursorIndex = result.data.students.length - 1
+        console.log(cursorIndex)
+        setStudentData((preStudentData: studentType[]) => [
+          ...preStudentData,
+          ...result.data.students
+        ])
+        setPostVariable((prePostVariable: postVariableType) => ({
+          ...prePostVariable,
+          page: result.data.meta.page,
+          cursor: result.data.students[cursorIndex].id,
+          hasNextPage: result.data.meta.hasNextPage
+        }))
+      }
     })
   }
 
@@ -164,6 +166,7 @@ export default function StudentPage() {
     }
   }, [infiniteScrollCount])
 
+  console.log(infiniteScrollCount)
   return (
     <div className="w-full 2xl:px-12 xl:px-12 lg:px-6 md:px-12 px-6">
       {/* 수강생 관리 + 수강생 등록 버튼 */}
@@ -230,7 +233,9 @@ export default function StudentPage() {
         </div>
         {/* 수강생 목록 시작 */}
         <div className="w-full flex flex-col gap-[14px]">
-          {searchInput !== '' && studentData.length == 0 ? (
+          {searchInput !== '' &&
+          studentData.length == 0 &&
+          infiniteScrollCount !== 0 ? (
             <div className="flex w-full h-screen justify-center items-center">
               <div className="flex flex-col gap-6 w-[432px] h-[244px]">
                 <Image
@@ -289,7 +294,7 @@ export default function StudentPage() {
           </div>
         </div>
       )}
-      <div id="test"></div>
+      {studentData.length !== 0 ? <div id="test"></div> : null}
     </div>
   )
 }
