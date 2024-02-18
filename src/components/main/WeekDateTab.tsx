a77e33b2e31aadbcd83b3ba5e0f45c7e73da0d31
shcho <acckr0604@gmail.com>
2024-02-18 20:08:19 +0900

    feat: mainPage-week datePicker 구현

--- src/components/main/WeekDateTab.tsx
+++ src/components/main/WeekDateTab.tsx
@@ -5,6 +5,8 @@ import chevronRight from '../../assets/icons/chevron/chevron-right.svg'
 import calender from '../../assets/icons/calendar.svg'
 import { useState, useEffect } from 'react'
 import DateSlideTab from './DateSlideTab'
+import WeekDatePicker from '../datePicker/weekDatePicker'
+import { dateDataType } from '../datePicker/dayDatePicker'
 
 export default function WeekDateTab() {
   const [dayData, setDayData] = useState<any>([])
@@ -12,168 +14,118 @@ export default function WeekDateTab() {
   const currentDate = new Date()
   const [dateData, setDateData] = useState({
     year: currentDate.getFullYear(),
-    month: currentDate.getMonth() + 1,
+    month: currentDate.getMonth(),
     date: currentDate.getDate()
   })
-  //console.log('dateData', dateData)
+  const dayName = [
+    '일요일',
+    '월요일',
+    '화요일',
+    '수요일',
+    '목요일',
+    '금요일',
+    '토요일'
+  ]
+  const [weekData, setWeekData] = useState<number>(0)
+  const [weekLength, setWeekLength] = useState<{
+    lastMonthLength: number
+    currentMonthLength: number
+  }>({
+    lastMonthLength: 0,
+    currentMonthLength: 0
+  })
+  const [activeDay, setActiveDay] = useState(dateData.date)
+  const [isClickedDatePicker, setIsClickedDatePicker] = useState<boolean>(false)
+  const [isClickedArrow, setIsClickedArrow] = useState<boolean>(false)
+  const [clickArrowCount, setClickArrowCount] = useState(0)
 
-  let [week, setWeek] = useState<number>(0)
-  let [activeDay, setActiveDay] = useState(dateData.date)
+  const onClickDatePickerHandler = () => {
+    setIsClickedDatePicker(prev => !prev)
+  }
 
   function clickDayTab(e: any) {
-    //console.log(e.currentTarget.id)
+    const date = Number(e.currentTarget.id)
+    setDateData(prevDateData => ({
+      ...prevDateData,
+      date: date
+    }))
     setActiveDay(e.currentTarget.id)
   }
 
-  //console.log(currentDate.getDay())
-  useEffect(() => {
-    function calculateWeekNum() {
-      let calculateEarlyMonthVar: number
-      if (dateData.month == 1) {
-        calculateEarlyMonthVar = 0
-      } else {
-        calculateEarlyMonthVar = dateData.month - 1
-      }
-      let calculateEndMonthVar: number = dateData.month
-
-      const lastMonthLastDateData = new Date(
-        dateData.year,
-        calculateEarlyMonthVar,
-        0
-      )
-
-      const currentMonthFirstDateDate = new Date(
-        dateData.year,
-        calculateEarlyMonthVar,
-        1
-      )
-      const currentMonthLastDateData = new Date(
-        dateData.year,
-        calculateEndMonthVar,
-        0
-      )
-
-      //console.log('currentMonth', currentMonthFirstDateDate)
-      //console.log('nextMonth', currentMonthLastDateData)
-
-      let date = []
-      for (var i = 1; i <= currentMonthLastDateData.getDate(); i++) {
-        date.push(i)
-      }
-      const firstWeekLength = 7 - currentMonthFirstDateDate.getDay()
-
-      let weekData = []
-      weekData.push(date.slice(0, firstWeekLength))
-      for (
-        var i = firstWeekLength;
-        i < currentMonthLastDateData.getDate();
-        i += 7
-      ) {
-        weekData.push(date.slice(i, i + 7))
-      }
-
-      const lastMonthEndDate = lastMonthLastDateData.getDate()
-      const diffFirstWeekLength = 7 - weekData[0].length
-      const diffLastWeekLength = 7 - weekData[weekData.length - 1].length
-
-      for (
-        var i = lastMonthEndDate;
-        i > lastMonthEndDate - diffFirstWeekLength;
-        i--
-      ) {
-        weekData[0].unshift(i)
-      }
-
-      for (var i = 1; i <= diffLastWeekLength; i++) {
-        weekData[weekData.length - 1].push(i)
-      }
-
-      return weekData
-    }
-    let weekData = calculateWeekNum()
-
-    let weekAndDateData: any = []
-    const dayName = [
-      '일요일',
-      '월요일',
-      '화요일',
-      '수요일',
-      '목요일',
-      '금요일',
-      '토요일'
-    ]
-    for (var i = 0; i < weekData.length; i++) {
-      let array: { week: string; data: { day: string; date: number }[] } = {
-        week: `${i + 1}`,
-        data: []
-      }
-      for (var j = 0; j < weekData[i].length; j++) {
-        array.data.push({
-          day: dayName[j],
-          date: weekData[i][j]
-        })
-      }
-      weekAndDateData.push(array)
-    }
-    //console.log(weekAndDateData)
-
-    function checkWeekNum(date: number): any {
-      for (var i = 0; i < weekData.length; i++) {
-        if (weekData[i].includes(date)) {
-          return Number(i + 1)
-        }
-      }
-    }
-
-    let week = checkWeekNum(dateData.date)
-    /* 현재 연도 == dateData.year && 현재 월 == dateData.month */
-    if (
-      currentDate.getFullYear() == dateData.year &&
-      currentDate.getMonth() + 1 == dateData.month
-    ) {
-      setWeek(week)
-    } else if (
-      currentDate.getFullYear() == dateData.year &&
-      dateData.month < currentDate.getMonth() + 1
-    ) {
-      /* dateData.month < 현재 월 */
-      setWeek(weekData.length)
-    }
-    setDayData(weekAndDateData)
-    //console.log('newDayData', newDayData)
-  }, [dateData.month])
-
-  //console.log(dayData)
-
   function moveForwardWeek() {
-    let standard = dayData.length
-    if (week === standard) {
-      if (dateData.month == 12) {
+    const maxWeek = weekLength.currentMonthLength
+    setClickArrowCount(clickArrowCount => (clickArrowCount += 1))
+    if (weekData === maxWeek) {
+      if (dateData.month === 11) {
         setDateData({
           ...dateData,
           year: dateData.year + 1,
-          month: 1
+          month: 0,
+          date: 1
         })
-        setWeek(1)
       } else {
         setDateData({
           ...dateData,
-          month: dateData.month + 1
+          month: dateData.month + 1,
+          date: 1
         })
-        setWeek(1)
       }
+      setActiveDay(1)
+      setWeekData(1)
     } else {
-      setWeek(week + 1)
+      setWeekData(weekData + 1)
+      setActiveDay(dayData[weekData].date[0].date)
+      setDateData(prevDateData => ({
+        ...prevDateData,
+        date: Number(dayData[weekData].date[0].date)
+      }))
     }
+    setIsClickedArrow(true)
+    setIsClickedDatePicker(false)
   }
 
   function moveBackWeek() {
-    if (week == 1) {
-      if (dateData.month == 1) {
+    setClickArrowCount(clickArrowCount => (clickArrowCount += 1))
+    if (weekData === 1) {
+      setWeekData(weekLength.lastMonthLength)
+      if (dateData.month === 0) {
+        setDateData(prevDateData => ({
+          ...prevDateData,
+          year: dateData.year - 1,
+          month: 11,
+          date: 31
+        }))
+        setActiveDay(31)
+      } else {
+        setDateData(prevDateData => ({
+          ...prevDateData,
+          month: dateData.month - 1,
+          date: new Date(dateData.year, dateData.month, 0).getDate()
+        }))
+        setActiveDay(new Date(dateData.year, dateData.month, 0).getDate())
+      }
+      /* if (dateData.month === 0) {
+        setActiveDay(31)
+      } else {
+        setActiveDay(new Date(dateData.year, dateData.month, 0).getDate())
+      } */
+    } else {
+      setDateData(prevDateData => ({
+        ...prevDateData,
+        date: Number(dayData[weekData - 2].date[6].date)
+      }))
+      setActiveDay(dayData[weekData - 2].date[6].date)
+      setWeekData(weekData - 1)
+    }
+    setIsClickedArrow(true)
+    setIsClickedDatePicker(false)
+    /* if (weekData === 1) {
+      setWeekData(weekLength.lastMonthLength)
+      if (dateData.month === 0) {
         setDateData({
           ...dateData,
           year: dateData.year - 1,
-          month: 12
+          month: 11
         })
       } else {
         setDateData({
@@ -181,13 +133,86 @@ export default function WeekDateTab() {
           month: dateData.month - 1
         })
       }
-      //console.log(dayData)
     } else {
-      setWeek(week - 1)
-    }
+      setWeekData(weekData - 1)
+    } */
+  }
+  const setDateDataFromChild = (data: dateDataType, type: string) => {
+    setDateData({
+      ...dateData,
+      year: data.year,
+      month: data.month
+    })
+    setIsClickedDatePicker(false)
   }
 
-  //console.log(dayData)
+  const setWeekDataFromChild = (week: number) => {
+    setWeekData(week)
+  }
+
+  const getCalanderData = (monthData: number) => {
+    const lastDateOfLastMonthData = new Date(dateData.year, monthData, 0)
+    const firstDateOfCurrentMonthData = new Date(dateData.year, monthData)
+    const lastDateOfCurrnetMonthData = new Date(dateData.year, monthData + 1, 0)
+    const firstDateOfNextMonthData = new Date(dateData.year, monthData + 1)
+
+    let list = []
+    if (firstDateOfCurrentMonthData.getDay() !== 0) {
+      for (var i = lastDateOfLastMonthData.getDay(); i >= 0; i--) {
+        list.push({
+          date: `${lastDateOfLastMonthData.getDate() - i}`
+        })
+      }
+    }
+
+    for (var i = 1; i <= lastDateOfCurrnetMonthData.getDate(); i++) {
+      list.push({ date: `${i}` })
+    }
+
+    if (lastDateOfCurrnetMonthData.getDay() !== 6) {
+      for (var i = 1; i <= 7 - firstDateOfNextMonthData.getDay(); i++) {
+        list.push({
+          date: `${i}`
+        })
+      }
+    }
+
+    let result = []
+    let weekNumber = 1
+    for (var i = 0; i < list.length; i += 7) {
+      result.push({ date: list.slice(i, i + 7), week: weekNumber })
+      weekNumber++
+    }
+
+    return result
+  }
+
+  useEffect(() => {
+    const lastWeekList = getCalanderData(dateData.month - 1)
+    const currentWeekList = getCalanderData(dateData.month)
+    setWeekLength(prev => ({
+      ...prev,
+      lastMonthLength: lastWeekList.length,
+      currentMonthLength: currentWeekList.length
+    }))
+    if (
+      currentDate.getFullYear() === dateData.year &&
+      currentDate.getMonth() === dateData.month &&
+      clickArrowCount === 0
+    ) {
+      for (var i = 0; i < currentWeekList.length; i++) {
+        for (var j = 0; j < currentWeekList[i].date.length; j++) {
+          if (currentWeekList[i].date[j].date === dateData.date.toString()) {
+            setWeekData(currentWeekList[i].week)
+          }
+        }
+      }
+    }
+    setDayData(currentWeekList)
+  }, [dateData.month])
+
+  /* post 할 데이터 dateData 바꾸기 */
+  console.log(dateData)
   return (
     <>
       <div className="mt-[80px] w-full flex xl:mx-auto xl:max-w-[1016px] lg:max-w-[936px]">
@@ -203,11 +228,12 @@ export default function WeekDateTab() {
             >
               <Image src={chevronLeft} width={24} height={24} alt=" " />
             </div>
-            <div className="w-full px-3 py-2 flex justify-center gap-2 items-center">
+            <div
+              className="w-full px-3 py-2 flex justify-center gap-2 items-center gray-900-semibold text-base font-['Pretendard'] cursor-pointer hover:text-primary-600"
+              onClick={onClickDatePickerHandler}
+            >
               <Image src={calender} width={18} height={18} alt=" " />
-              <span className="gray-900-semibold text-base font-['Pretendard']">
-                {dateData.year}년 {dateData.month}월 {week}주차
-              </span>
+              {dateData.year}년 {dateData.month + 1}월 {weekData}주차
             </div>
             <div
               className="h-full w-10 border p-1 rounded border-gray-200 bg-white flex items-center cursor-pointer"
@@ -217,39 +243,50 @@ export default function WeekDateTab() {
             </div>
           </div>
           <DateSlideTab />
+          {isClickedDatePicker && (
+            <div className="absolute w-[255px] z-10 right-0 left-0 mx-auto top-[60px]">
+              <WeekDatePicker
+                changeParentsDateData={setDateDataFromChild}
+                changeParentsWeekData={setWeekDataFromChild}
+                parentsDateData={dateData}
+                parentsWeekData={weekData}
+              />
+            </div>
+          )}
         </div>
       </div>
       {/* 요일 선택 탭 */}
       <div className="w-full flex max-w-[1016px] mx-auto justify-end pt-[32px]">
         <div className="w-[51px] xl:mr-5 lg:mr-4"></div>
         <div className="w-full grid grid-cols-7 gap-[7px]">
-          {dayData[week - 1]?.data.map((data: any, i: number) => {
+          {dayData[weekData - 1]?.date.map((data: any, i: number) => {
             return (
               <div
                 key={i}
                 id={data.date}
-                className={`xl:max-w-[129px] lg:max-w-[119px] h-full px-3 py-2 flex flex-col  border rounded-lg ${
+                className={`xl:max-w-[129px] lg:max-w-[119px] h-full px-3 py-2 flex flex-col border rounded-lg bg-white cursor-pointer
+                ${
                   activeDay == data.date
                     ? 'border-primary-600'
                     : 'border-gray-200'
-                } bg-white cursor-pointer`}
+                }`}
                 onClick={clickDayTab}
               >
                 <div
-                  className={`${
+                  className={`text-center text-sm font-medium ${
                     activeDay == data.date
                       ? 'text-primary-600'
                       : 'text-gray-400'
-                  } text-center text-sm font-medium`}
+                  }`}
                 >
-                  {data.day}
+                  {dayName[i]}
                 </div>
                 <div
-                  className={`${
+                  className={`text-center text-xl font-bold ${
                     activeDay == data.date
                       ? 'text-primary-600'
                       : 'text-gray-400'
-                  } text-center text-xl font-bold`}
+                  }`}
                 >
                   {data.date}
                 </div>
