1e7f288c774b7d613bcd94cc21fc2d5f4248ef93
shcho <acckr0604@gmail.com>
2024-04-06 17:39:51 +0900

    [dev] [View]: 청구/납부 UI 구현

--- src/app/(nav)/pay/page.tsx
+++ src/app/(nav)/pay/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 
 import SearchInput from '@/components/common/SearchInput'
 import { studentType, metaType } from '../student/page'
@@ -7,14 +7,21 @@ import ListInfo from '@/components/view/ListInfo'
 import instance from '@/lib/api/axios'
 import NoneResult from '@/components/common/NoneResult'
 import List from '@/components/view/List'
+import Loading from '@/components/common/Loading'
 
 export default function PayPage() {
+  const target = useRef<HTMLDivElement>(null)
+
   const [studentList, setStudentList] = useState<studentType[]>([])
-  const [metaData, setMetaData] = useState<metaType[]>([])
+  const [metaData, setMetaData] = useState<metaType>({
+    page: 1,
+    hasNextPage: false
+  })
   const [inputData, setInputData] = useState<{ value: string; searchBy: string }>({
     value: '',
     searchBy: ''
   })
+  const [isLoading, setIsLoading] = useState<boolean>(false)
   const [isRefresh, setIsRefresh] = useState<boolean>(false)
   const listInfoProps = ['이름', '전화번호', '수강중인 클래스', '결제상태']
 
@@ -47,13 +54,74 @@ export default function PayPage() {
     })
   }, [])
 
+  useEffect(() => {
+    if (metaData.hasNextPage) {
+      const options = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 1.0
+      }
+
+      const getData = () => {
+        if (inputData.value === '') {
+          instance(`/students?searchBy=none&page=${metaData.page + 1}`).then(res => {
+            const studentsData = res.data.data.students
+            const meta = res.data.data.meta
+            setStudentList(prev => [...prev, ...studentsData])
+            setMetaData(prev => ({
+              ...prev,
+              page: meta.page,
+              hasNextPage: meta.hasNextPage
+            }))
+            setIsLoading(false)
+          })
+        } else {
+          instance(
+            `/students?searchBy=${inputData.searchBy}&${inputData.searchBy}=${inputData.value}&page=${
+              metaData.page + 1
+            }`
+          ).then(res => {
+            const studentsData = res.data.data.students
+            const meta = res.data.data.meta
+            setStudentList(prev => [...prev, ...studentsData])
+            setMetaData(prev => ({
+              ...prev,
+              page: meta.page,
+              hasNextPage: meta.hasNextPage
+            }))
+            setIsLoading(false)
+          })
+        }
+      }
+
+      const callback = (entry: any) => {
+        if (entry[0].isIntersecting) {
+          setIsLoading(true)
+          setTimeout(() => {
+            getData()
+          }, 500)
+        }
+      }
+      const observer = new IntersectionObserver(callback, options)
+      if (target.current) {
+        observer.observe(target.current)
+      }
+
+      return () => {
+        if (observer && target.current) {
+          observer.unobserve(target.current)
+        }
+      }
+    }
+  }, [metaData])
+
   return (
     <div className="w-full h-full px-6 md:px-12 lg:px-6 xl:px-12 py-[60px] box-border">
       <div className="flex w-full justify-between items-center mb-[30px]">
         <div className="black-bold text-3xl">청구/납부</div>
       </div>
       <div className="w-full flex justify-end">
-        <SearchInput type="studnets" passInputData={getInputDataFromChild} />
+        <SearchInput type="students" passInputData={getInputDataFromChild} />
       </div>
       <div className="w-full flex flex-col gap-3">
         <ListInfo type="pay" listInfo={listInfoProps} />
@@ -65,6 +133,8 @@ export default function PayPage() {
           })}
         </div>
       </div>
+      {!isLoading && <div ref={target}></div>}
+      {isLoading && <Loading />}
     </div>
   )
 }
