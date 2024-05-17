ae8b5df8d1ae9b55874541592b05fa5942f74c1d
shchoon <acckr0604@gmail.com>
2024-05-17 23:47:24 +0900

    [dev] redirect test

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/home',
+        permanent: true
+      }
+    ]
+  },
   webpack: config => {
     config.module.rules.push({
       test: /\.svg$/,
