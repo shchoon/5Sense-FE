79c98d8876309d4113e95850b3a9b957441dad42
shchoon <acckr0604@gmail.com>
2024-05-17 15:35:09 +0900

    test

--- src/app/(nav)/room/modifyRoom/[id]/page.tsx
+++ src/app/(nav)/room/modifyRoom/[id]/page.tsx
@@ -1,9 +1,5 @@
 import Test from '@/components/test'
 
-export function generateStaticParams() {
-  return [{ id: '1' }, { id: '2' }]
-}
-
 export default function ModifyRoom({ params }: { params: { id: string } }) {
   const roomId = params.id
   return <Test id={roomId} />
