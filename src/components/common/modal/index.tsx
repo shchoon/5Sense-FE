f356bebb706abc22f7dea950d05e46356d047527
yeun38 <choh1997@naver.com>
2024-05-28 23:48:18 +0900

    모달 리팩토링

--- src/components/common/modal/index.tsx
+++ src/components/common/modal/index.tsx
@@ -1,25 +1,53 @@
-import React from 'react'
+'use client'
+import { modalState } from '@/lib/state/modal'
+import { useState } from 'react'
 import ReactDOM from 'react-dom'
+import { useSetRecoilState } from 'recoil'
 
 interface IProps {
   onClose?: () => void
   children: JSX.Element
-  small?: boolean
-  props?: {
-    id: number
-    type: string
-  }
+  isOpen: boolean
 }
 
-export default function Modal({ children, small, props }: IProps) {
+// 모달 컴포넌트
+const Modal = ({ isOpen, onClose, children }: IProps) => {
+  if (!isOpen) return null
+
   return ReactDOM.createPortal(
     <div className="absolute h-screen w-full bg-black bg-opacity-50 inset-y-0 inset-x-0 z-50">
-      {small ? (
-        <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] ">{children}</div>
-      ) : (
-        <div className="w-[480px] h-screen">{children}</div>
-      )}
+      <div onClick={onClose}></div>
+      <div className="w-[480px] h-screen bg-white">{children}</div>
     </div>,
     document.body
   )
 }
+
+// useModal 훅
+const useModal = () => {
+  const [isOpen, setIsOpen] = useState(false)
+  const setModal = useSetRecoilState(modalState)
+
+  const openModal = () => {
+    setIsOpen(true)
+    setModal(true)
+  }
+  const closeModal = () => {
+    setIsOpen(false)
+    setModal(false)
+  }
+
+  const ModalComponent = ({ children }: { children: JSX.Element }) => (
+    <Modal isOpen={isOpen} onClose={closeModal}>
+      {children}
+    </Modal>
+  )
+
+  return {
+    openModal,
+    closeModal,
+    Modal: ModalComponent
+  }
+}
+
+export default useModal
