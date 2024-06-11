eb2520cb6b1628c06eaca8d2db5c4b0e2e5b7795
shchoon <acckr0604@gmail.com>
2024-06-11 15:54:38 +0900

    [Chore]: modifyRoom dynamicRouting 제거

--- src/app/(service)/(nav)/room/modifyRoom/page.tsx
+++ src/app/(service)/(nav)/room/modifyRoom/page.tsx
@@ -5,10 +5,8 @@ import { useEffect, useState } from 'react'
 import { usePathname, useRouter } from 'next/navigation'
 import { useRecoilValue, useSetRecoilState } from 'recoil'
 
-import InputForm, { InputFormProps } from '@/components/common/InputForm'
-import useInputNum from '@/hooks/useInputNum'
-import { InputNumProps } from '@/app/(service)/(nav)/student/register/page'
 import instance from '@/lib/api/axios'
+import { RoomDetailsState } from '@/lib/state/roomDetails'
 import ContentHeader from '@/components/common/ContentHeader'
 
 import ArrowBackIcon from 'public/assets/icons/allowBack.svg'
@@ -17,26 +15,23 @@ import MinusIcon from 'public/assets/icons/minus_vector.svg'
 import PlusIcon from 'public/assets/icons/plus_vector.svg'
 
 export default function ModifyRoom() {
+  const roomDetails = useRecoilValue(RoomDetailsState)
+  const setRoomDetails = useSetRecoilState(RoomDetailsState)
   const router = useRouter()
-  //const pathName = usePathname()
+  //const roomId = id
   //const [inputValue, setInputValue] = useState<string>(roomName)
-  const [roomData, setRoomData] = useState<{ name: string; roomId: number; capacity: number }>({
-    name: (typeof window !== undefined && localStorage.getItem('roomName')) || '',
-    roomId: (typeof window !== undefined && Number(localStorage.getItem('roomId'))) || 0,
-    capacity: (typeof window !== undefined && Number(localStorage.getItem('capacity'))) || 0
-  })
 
   const handleChangeRoomName = (e: React.ChangeEvent<HTMLInputElement>) => {
     let value: string = e.target.value
     const maxLength: number = e.target.maxLength
     if (value.length > maxLength) {
-      setRoomData(prev => ({
+      setRoomDetails(prev => ({
         ...prev,
         name: value.slice(0, maxLength)
       }))
     }
 
-    setRoomData(prev => ({
+    setRoomDetails(prev => ({
       ...prev,
       name: value
     }))
@@ -54,10 +49,29 @@ export default function ModifyRoom() {
     submitData: inputValue,
     setSubmitData: setInputValue
   } */
+  useEffect(() => {
+    const date = new Date()
+    const roomId = localStorage.getItem('roomId') as string
+    instance('lesson-rooms/daily', {
+      params: {
+        date: new Date(date.getFullYear(), date.getMonth(), date.getDate()).toISOString()
+      }
+    }).then(res => {
+      console.log(res)
+      const room = res.data.data.filter((data: { id: number }) => data.id === Number(roomId))
+      console.log(room)
+      setRoomDetails(prev => ({
+        ...prev,
+        id: room[0].id,
+        name: room[0].name,
+        capacity: room[0].capacity
+      }))
+    })
+  }, [])
 
   return (
     <div className='w-full flex flex-col items-center pb-[60px]'>
-      <ContentHeader title='강의실 수정' back onClick={() => router.push('/room')} />
+        <ContentHeader title='강의실 수정' back onClick={() => router.push('/room')} />
       <div className="w-[640px] flex flex-col gap-[34px]">
         <div className="w-full px-6 py-8 flex flex-col gap-10 border rounded-xl border-1 border-gray-200">
           <div className="gray-900-bold text-xl">강의실 정보</div>
@@ -66,13 +80,15 @@ export default function ModifyRoom() {
               <div className="w-full text-left gray-800-semibold text-base">강의실 이름</div>
               <input
                 className="input-line-gray"
-                value={roomData.name}
+                value={roomDetails.name}
                 maxLength={20}
                 onChange={e => {
                   handleChangeRoomName(e)
                 }}
               />
-              <div className="w-full text-right gray-500-normal text-sm font-['Inter']">{roomData.name.length}/20</div>
+              <div className="w-full text-right gray-500-normal text-sm font-['Inter']">
+                {roomDetails.name.length}/20
+              </div>
             </div>
             <div className="w-full flex flex-col gap-2">
               <div className="w-full gray-800-semibold text-base">권장 허용 인원</div>
@@ -80,11 +96,11 @@ export default function ModifyRoom() {
                 <button
                   type="button"
                   className={`w-10 h-full flex items-center justify-center rounded-full ${
-                    roomData.capacity === 1 ? 'bg-gray-200' : 'bg-primary-600'
+                    roomDetails.capacity === 1 ? 'bg-gray-200' : 'bg-primary-600'
                   }`}
                   onClick={() => {
-                    if (roomData.capacity !== 1) {
-                      setRoomData(prev => ({
+                    if (roomDetails.capacity !== 1) {
+                      setRoomDetails(prev => ({
                         ...prev,
                         capacity: prev.capacity - 1
                       }))
@@ -94,13 +110,13 @@ export default function ModifyRoom() {
                   <MinusIcon alt="Minus" />
                 </button>
                 <div className="w-[472px] h-full flex items-center justify-center text-lg gray-800-semibold">
-                  {roomData.capacity}명
+                  {roomDetails.capacity}명
                 </div>
                 <button
                   type="button"
                   className="w-10 h-full flex items-center justify-center rounded-full bg-primary-600"
                   onClick={() => {
-                    setRoomData(prev => ({
+                    setRoomDetails(prev => ({
                       ...prev,
                       capacity: prev.capacity + 1
                     }))
@@ -116,12 +132,11 @@ export default function ModifyRoom() {
           className="w-full h-[52px] flex justify-center items-center text-white text-base font-semibold btn-purple"
           onClick={() => {
             instance
-              .put(`/lesson-rooms/${roomData.roomId}`, {
-                name: roomData.name,
-                capacity: roomData.capacity
+              .put(`/lesson-rooms/${roomDetails.id}`, {
+                name: roomDetails.name,
+                capacity: roomDetails.capacity
               })
               .then(res => {
-                console.log(res)
                 router.push('/room')
               })
           }}
