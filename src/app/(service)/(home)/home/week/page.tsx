1b6301d068d79e1c4debcfacecc4272281c9de4b
shchoon <acckr0604@gmail.com>
2024-06-16 02:16:26 +0900

    [feat]: 메인페이지 주 -> 회차반 사이드 모달 구현

--- src/app/(service)/(home)/home/week/page.tsx
+++ src/app/(service)/(home)/home/week/page.tsx
@@ -8,32 +8,47 @@ import { WeekCalendarDateState } from '@/lib/state/calendar/WeekCalendarDateStat
 import { Drawer } from 'flowbite-react'
 import { DetailClassState } from '@/lib/state/detailClassState'
 import instance from '@/lib/api/axios'
+import { changePhoneNUmberToString } from '@/utils'
 
 export default function MainPageWeek() {
   const detailClassState = useRecoilValue(DetailClassState)
   const setDetailClassState = useSetRecoilState(DetailClassState)
   const weekdata = useRecoilValue(WeekCalendarDateState)
-  const [isOpen, setIsOpen] = useState(true)
+  const [isOpen, setIsOpen] = useState(false)
 
   const handleClose = () => setIsOpen(false)
 
-  const classDetail = {
-    type: 'duration',
+  const [classDetail,setClassDetail] = useState({
+    type: '',
     categoryName: '수영',
-    className: '수영 초급반',
-    capacity: 5,
-    teacher: '박태환',
-    memo: '메모메모메모메모메모메모메모메모'
-  }
+    className: '',
+    capacity: 0,
+    teacher: '',
+    memo: '',
+    students: []
+  })
 
   useEffect(() => {
     if (detailClassState.id !== 0) {
       instance.get(`/${detailClassState.type}-lessons/${detailClassState.id}/details`).then(res => {
         console.log(res)
+        const classData = res.data.data
+        setClassDetail(prev => ({
+          ...prev,
+          type: classData.type,
+          categoryName: classData.category.subName,
+          className: classData.name,
+          capacity: classData.capacity,
+          teacher: classData.teacher.name,
+          memo: classData.memo,
+          students: classData.registeredStudents
+        }))
       })
+      setIsOpen(true)
     }
   }, [detailClassState])
 
+  console.log(detailClassState)
   return (
     <>
       <WeekCalendar />
@@ -79,10 +94,12 @@ export default function MainPageWeek() {
                   <div className="w-full gray-800-medium text-base">회차반</div>
                 )}
               </div>
-              <div className="w-full flex gap-8">
+              {classDetail.type === 'session' && <div className="w-full flex gap-8">
                 <div className="w-[150px] gray-500-medium text-base">• 최대 수업 정원</div>
                 <div className="w-full gray-800-medium text-base">{classDetail.capacity}</div>
-              </div>
+              </div>}
+              {classDetail.type === 'duration' && 
+              <>
               <div className="w-full flex gap-8">
                 <div className="w-[150px] gray-500-medium text-base">• 기간 정보</div>
                 <div className="w-full gray-800-medium text-base">
@@ -100,11 +117,36 @@ export default function MainPageWeek() {
                     )
                   })} */}
               </div>
+              </>}
               <div className="w-full flex gap-8">
                 <div className="w-[150px] gray-500-medium text-base">• 클래스 메모</div>
                 <div className="w-full gray-800-medium text-base">{classDetail.memo}</div>
               </div>
             </div>
+            {/* 회원 목록 */}
+        <div className="w-full flex flex-col gap-4 p-6 border border-1 border-gray-200 rounded-lg shadow">
+          <div className="w-full flex gap-1.5">
+            <div className="text-gray-800 text-base font-bold">회원 목록</div>
+            <div className="gray-800-semibold text-base">({classDetail.students.length})</div>
+          </div>
+          {/* 학생 리스트 */}
+          <div className="w-full flex flex-col gap-2.5">
+            {classDetail.students.map((data: any, index: number) => {
+              return (
+                <div key={index} className="w-full flex gap-1">
+                  <span className="w-[22px] text-primary-600 text-sm font-bold">{index + 1}</span>
+                  <div className="w-full flex gap-2.5">
+                    <div className="gray-800-medium text-sm">{data.name}</div>
+                    <div className="w-[102px] gray-500-normal text-sm">{changePhoneNUmberToString(data.phone)}</div>
+                    {classDetail.type === 'session' && (
+                      <div className="text-primary-600 text-xs font-semibold">(잔여회차 : {data.sessionCount})</div>
+                    )}
+                  </div>
+                </div>
+              )
+            })}
+          </div>
+        </div>
           </div>
         </Drawer.Items>
       </Drawer>
