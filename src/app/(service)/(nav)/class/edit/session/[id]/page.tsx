88ad95107ae3ef9025e4c9bda625ba027787e24b
shchoon <acckr0604@gmail.com>
2024-06-24 14:26:41 +0900

    [beta-test] test

--- src/app/(service)/(nav)/class/edit/session/[id]/page.tsx
+++ src/app/(service)/(nav)/class/edit/session/[id]/page.tsx
@@ -13,7 +13,6 @@ import instance from '@/lib/api/axios'
 import { access } from 'fs'
 import { getStaticPaths } from 'next/dist/build/templates/pages'
 
-
 export interface IClassInfo {
   name: string
   memo: string
@@ -52,20 +51,20 @@ export interface ITeacherInfo {
   name: string
 }
 
-export const dynamicParams = false;
+export const dynamicParams = true
 
 export async function generateStaticParams() {
-  const dynamicRoute = []
+  /* const dynamicRoute = []
   for(var i=1; i<100; i++){
     dynamicRoute.push({id: String(i)})
-  }
-  return dynamicRoute
+  } */
+  return []
 }
 
-/* const getSessionClassId = async () => {
-  const IP_ADDRESS = process.env.NEXT_PUBLIC_IP_ADDRESS
+const getSessionClassId = async (params: { id: string }) => {
+  //const IP_ADDRESS = process.env.NEXT_PUBLIC_IP_ADDRESS
   const accessToken = typeof window !== 'undefined' && localStorage.getItem('acccessToken')
-  const res = await fetch(`${IP_ADDRESS}/lessons/filters?type=session&page=1&take=10`,{
+  const res = await fetch(`/lessons/filters?type=session&page=1&take=10`, {
     method: 'GET',
     headers: {
       authorization: `Bearer ${accessToken}`
@@ -76,12 +75,12 @@ export async function generateStaticParams() {
 
   const sessions = await res.data.lessons
 
-  return sessions.map((lessons: {id: string}) => ({
+  return sessions.map((lessons: { id: string }) => ({
     id: String(lessons.id)
   }))
-} */
+}
 
-export default async function EditPage({params}: {params: {id: string}}) {
+export default async function EditPage(params: { id: string }) {
   //const classId = await getSessionClassId()
   console.log('params', params.id)
   return <SessionEdit classId={params.id} />
