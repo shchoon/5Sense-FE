09343815e0feef9bb2030a41a0844bb0be55fadb
shchoon <acckr0604@gmail.com>
2024-08-02 18:52:45 +0900

    [deploy] nextjs rewrites test

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const { type } = require('os')
+
 const nextConfig = {
   images: {
     remotePatterns: [
@@ -15,6 +17,236 @@ const nextConfig = {
       use: ['@svgr/webpack']
     })
     return config
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/students',
+        has: [
+          {
+            type: 'query',
+            key: 'take',
+            value: '(?<take>.*)'
+          },
+          {
+            type: 'query',
+            key: 'page',
+            value: '(?<page>.*)'
+          },
+          {
+            type: 'query',
+            key: 'searchBy',
+            value: '(?<searchBy>.*)'
+          },
+          {
+            type: 'query',
+            key: 'name',
+            value: '(?<name>.*)'
+          },
+          {
+            type: 'query',
+            key: 'phone',
+            value: '(?<phone>.*)'
+          }
+        ],
+        destination: `http://43.203.231.217:3000/students?take=:take&page=:page&searchBy=:searchBy&name=:name&phone=:phone`
+      },
+      {
+        source: '/students/:path*',
+        destination: 'http://43.203.231.217:3000/students/:path*'
+      },
+      {
+        source: '/teachers',
+        has: [
+          {
+            type: 'query',
+            key: 'take',
+            value: '(?<take>.*)'
+          },
+          {
+            type: 'query',
+            key: 'page',
+            value: '(?<page>.*)'
+          },
+          {
+            type: 'query',
+            key: 'searchBy',
+            value: '(?<searchBy>.*)'
+          },
+          {
+            type: 'query',
+            key: 'name',
+            value: '(?<name>.*)'
+          },
+          {
+            type: 'query',
+            key: 'phone',
+            value: '(?<phone>.*)'
+          }
+        ],
+        destination: `http://43.203.231.217:3000/teachers?take=:take&page=:page&searchBy=:searchBy&name=:name&phone=:phone`
+      },
+      {
+        source: '/teachers/:path*',
+        destination: 'http://43.203.231.217:3000/teachers/:path*'
+      },
+      {
+        source: '/billing-payments',
+        has: [
+          {
+            type: 'query',
+            key: 'take',
+            value: '(?<take>.*)'
+          },
+          {
+            type: 'query',
+            key: 'page',
+            value: '(?<page>.*)'
+          },
+          {
+            type: 'query',
+            key: 'searchBy',
+            value: '(?<searchBy>.*)'
+          },
+          {
+            type: 'query',
+            key: 'name',
+            value: '(?<name>.*)'
+          },
+          {
+            type: 'query',
+            key: 'phone',
+            value: '(?<phone>.*)'
+          },
+          {
+            type: 'query',
+            key: 'PaymentStatus',
+            value: '(?<PaymentStatus>.*)'
+          }
+        ],
+        destination:
+          'http://43.203.231.217:3000/billing-payments?take=:take&page=:page&searchBy=:searchBy&name=:name&phone=:phone&PaymentStatus=:PaymentStatus'
+      },
+      {
+        source: '/billing-payments/:path*',
+        destination: 'http://43.203.231.217:3000/billing-payments/:path*'
+      },
+      {
+        source: '/lessons/filters',
+        has: [
+          {
+            type: 'query',
+            name: 'page',
+            value: '(?<page>.*)'
+          },
+          {
+            type: 'query',
+            name: 'take',
+            value: '(?<take>.*)'
+          },
+          {
+            type: 'query',
+            name: 'type',
+            value: '(?<type>.*)'
+          },
+          {
+            type: 'query',
+            name: 'teachers',
+            value: '(?<teachers>.*)'
+          },
+          {
+            type: 'query',
+            name: 'categories',
+            value: '(?<categories>.*)'
+          }
+        ],
+        destination: `http://43.203.231.217:3000/lessons/filters?page=:page&take=:take&type=:type&teachers=:teachers&categories=:categories`
+      },
+      {
+        source: '/lesson-rooms/daily',
+        has: [
+          {
+            type: 'query',
+            name: 'date',
+            value: '(?<date>.*)'
+          }
+        ],
+        destination: 'http://43.203.231.217:3000/lesson-rooms/daily?date=:date'
+      },
+      {
+        source: '/lesson-rooms/range',
+        has: [
+          {
+            type: 'query',
+            name: 'startDate',
+            value: '(?<startDate>.*)'
+          },
+          {
+            type: 'query',
+            name: 'endDate',
+            value: '(?<endData>.*)'
+          },
+          {
+            type: 'query',
+            name: 'repeatDate',
+            value: '(?<repeatDate>.*)'
+          }
+        ],
+        destination:
+          'http://43.203.231.217:3000/lesson-rooms/daily?startDate=:startDate&endDate=:endDate&repeatDate=:repeatDate'
+      },
+      {
+        source: '/lesson-rooms/:path*',
+        destination: 'http://43.203.231.217:3000/lesson-rooms/:path*'
+      },
+      {
+        source: '/lesson-categories/:path*',
+        destination: 'http://43.203.231.217:3000/lesson-categories/:path*'
+      },
+      {
+        source: '/lessons/:path*',
+        destination: `http://43.203.231.217:3000/lessons/:path*`
+      },
+      {
+        source: '/centers/:path*',
+        destination: `http://43.203.231.217:3000/centers/:path*`
+      },
+      {
+        source: '/auth/:path*',
+        destination: 'http://43.203.231.217:3000/auth/:path*'
+      },
+      {
+        source: '/duration-lessons/:path*',
+        destination: 'http://43.203.231.217:3000/duration-lessons/:path*'
+      },
+      {
+        source: '/session-lessons',
+        has: [
+          {
+            type: 'query',
+            name: 'isCheckRegistrationsCount',
+            value: '(?<isCheckRegistrationsCount>.*)'
+          }
+        ],
+        destination: 'http://43.203.231.217:3000/session-lessons?isCheckRegistrationsCount=:isCheckRegistrationsCount'
+      },
+      {
+        source: '/session-lessons/:path*',
+        destination: 'http://43.203.231.217:3000/session-lessons/:path*'
+      },
+      {
+        source: '/session-lesson-schedules/:path*',
+        destination: 'http://43.203.231.217:3000/session-lesson-schedules/:path*'
+      },
+      {
+        source: '/session-lesson-registrations/:path*',
+        destination: 'http://43.203.231.217:3000/session-lesson-registrations/:path*'
+      },
+      {
+        source: '/duration-lesson-registrations/:path*',
+        destination: 'http://43.203.231.217:3000/duration-lesson-registrations/:path*'
+      }
+    ]
   },
 
   reactStrictMode: false
